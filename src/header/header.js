53d2bfbe938e0ff4d7e10aaa5968d811e811747e
Luke Preston <lukejpreston@gmail.com>
2018-02-12 21:43:06 +0000

    can swap between manager and soundboard

--- src/header/header.js
+++ src/header/header.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Header = () => {
-  return <div>header</div>
+const Header = ({header}) => {
+  return <header>
+    <section className='hero is-dark'>
+      <div className='hero-body'>
+        <div className='container'>
+          <h1 className='title'>Blank String</h1>
+          <h2 className='subtitle'>Soundboard</h2>
+        </div>
+      </div>
+    </section>
+    <div className='container'>
+      <div className='tabs'>
+        <ul>
+          <li className={`is-${header.soundboard}`}><Link to='/'>Soundboard</Link></li>
+          <li className={`is-${header.manager}`}><Link to='/manager'>Manger</Link></li>
+        </ul>
+      </div>
+    </div>
+  </header>
 }
 
 export default Header
--- src/header/reducer.js
+++ src/header/reducer.js
@@ -1,3 +1,17 @@
+const locationChange = '@@router/LOCATION_CHANGE'
+
 export default (state, action) => {
+  state.header = state.header || {
+    soundboard: 'active',
+    manager: 'inactive'
+  }
+
+  if (action.type === locationChange) {
+    const pathname = action.payload.pathname
+    state.header = {
+      soundboard: pathname === '/' ? 'active' : 'inactive',
+      manager: pathname === '/manager' ? 'active' : 'inactive'
+    }
+  }
   return state
 }
